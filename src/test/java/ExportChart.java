ab7b362af3eef62e8a55aed9f643fe339e680400
RahulKumar <rahul@fusioncharts.com>
2018-06-07 22:41:26 +0530

    Final commit for v1.0.0-rc with pom.xml

--- src/test/java/ExportChart.java
+++ src/test/java/ExportChart.java
@@ -3,7 +3,7 @@ import com.fusioncharts.fusionexport.client.*;
 public class ExportChart {
     public static void main(String[] args) throws Exception {
 
-        String rootPath = System.getProperty("user.dir") + java.io.File.separator;
+        final String rootPath = System.getProperty("user.dir") + java.io.File.separator;
         String configPath = rootPath + "examples" + java.io.File.separator + "chart-config-file.json";
         ExportConfig config = new ExportConfig();
         config.set("chartConfig", configPath);
