5cb81b682156b101d84af3adcb97f32797509e6d
Poushali Guha <poushali@collabion.com>
2018-06-22 16:23:11 +0530

    samples added for amazons3 upload,ftp upload and different library export

--- src/main/java/com/fusioncharts/fusionexport/client/ExportDoneListener.java
+++ src/main/java/com/fusioncharts/fusionexport/client/ExportDoneListener.java
@@ -1,5 +1,7 @@
 package com.fusioncharts.fusionexport.client;
 
+import java.io.IOException;
+
 public interface ExportDoneListener {
-    void exportDone(ExportDoneData result, ExportException error);
+    void exportDone(ExportDoneData result, ExportException error) throws IOException;
 }
