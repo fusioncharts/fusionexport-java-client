4ad7f69f2984ec7895d5da5f8b5a8b919b6ca5b7
Ujjal Kumar Dutta <duttaujjal143@gmail.com>
2018-09-13 17:25:43 +0530

    Added config to create Fat Jar

--- src/main/java/com/fusioncharts/fusionexport/client/ExportManager.java
+++ src/main/java/com/fusioncharts/fusionexport/client/ExportManager.java
@@ -1,23 +1,12 @@
 package com.fusioncharts.fusionexport.client;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.GetBucketLocationRequest;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import org.apache.commons.net.ftp.FTP;
-import org.apache.commons.net.ftp.FTPClient;
-
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Base64;
+
 
 public class ExportManager {
 
