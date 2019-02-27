5df8263d16b1b15810a2e8324c9f7649a7913f6e
Jimut Dhali <jimutdhali@gmail.com>
2019-02-27 18:50:55 +0530

    Update build

--- examples/single_export_pdf.java
+++ examples/single_export_pdf.java
@@ -2,13 +2,11 @@ import com.fusioncharts.fusionexport.client.*;
 
 public class ExportChart {
     public static void main(String[] args) throws Exception {
-
         ExportManager em = new ExportManager();
         ExportConfig config = new ExportConfig();
 
         config.set("chartConfig", "fullPath_single.json");
         config.set("type", "pdf");
-
 
         em.export(config,"outPath",false);
     }
