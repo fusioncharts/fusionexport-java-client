292790d9f7bee6f76194db69d64e624ccb82946d
Rahul Kumar <rahul@fusioncharts.com>
2019-03-07 17:21:56 +0530

    Improved README.md and server error message

--- src/test/java/ExportChart.java
+++ src/test/java/ExportChart.java
@@ -2,22 +2,45 @@ import com.fusioncharts.fusionexport.client.*; // import sdk
 
 public class ExportChart {
     public static void main(String[] args) throws Exception {
-
 	
-        String configPath = "fullpath/resources/static2/resources/multiple.json";
-		configPath = "D:\\_Work@FusionCharts\\FusionExport_Code\\fusionexport-java-client\\examples\\resources\\multiple.json";
-        String templatePath ="fullpath/resources/static2/resources/template.html";
-		templatePath = "D:\\_Work@FusionCharts\\FusionExport_Code\\fusionexport-java-client\\examples\\resources\\template.html";
-
+		StringBuilder chartConf = new StringBuilder();
+		chartConf.append("[");
+		chartConf.append("  {");
+		chartConf.append("    \"type\": \"column2d\",");
+		chartConf.append("    \"renderAt\": \"chart-container\",");
+		chartConf.append("    \"width\": \"600\",");
+		chartConf.append("    \"height\": \"200\",");
+		chartConf.append("    \"dataFormat\": \"json\",");
+		chartConf.append("    \"dataSource\": {");
+		chartConf.append("      \"chart\": {");
+		chartConf.append("        \"caption\": \"Number of visitors last week\",");
+		chartConf.append("        \"subCaption\": \"Bakersfield Central vs Los Angeles Topanga\"");
+		chartConf.append("      },");
+		chartConf.append("      \"data\": [");
+		chartConf.append("        {");
+		chartConf.append("          \"label\": \"Mon\",");
+		chartConf.append("          \"value\": \"15123\"");
+		chartConf.append("        },");
+		chartConf.append("        {");
+		chartConf.append("          \"label\": \"Tue\",");
+		chartConf.append("          \"value\": \"14233\"");
+		chartConf.append("        },");
+		chartConf.append("        {");
+		chartConf.append("          \"label\": \"Wed\",");
+		chartConf.append("          \"value\": \"25507\"");
+		chartConf.append("        }");
+		chartConf.append("      ]");
+		chartConf.append("    }");
+		chartConf.append("  }");
+		chartConf.append("]");		
+		
+		
         // Instantiate the ExportManager class
 		ExportManager em = new ExportManager();
-		
         // Instantiate the ExportConfig class and add the required configurations
         ExportConfig config = new ExportConfig();
-        config.set("chartConfig", configPath);
-        config.set("templateFilePath", templatePath);
-        config.set("type", "pdf");
-        config.set("templateFormat", "A4");
+        config.set("chartConfig", chartConf.toString());
+        config.set("type", "png");
 		
         String[] files = em.export(config,".",true);
         
