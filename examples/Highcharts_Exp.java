4a67b1f7c244bea7a6297ce01cf074efed70ebbc
Rahul Kumar <rahul@fusioncharts.com>
2019-12-10 16:06:22 +0530

    Added more samples

---
+++ examples/Highcharts_Exp.java
@@ -0,0 +1,26 @@
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Dictionary;
+import java.util.HashMap;
+
+import com.fusioncharts.fusionexport.client.*; // import sdk
+
+public class Highcharts_Exp {
+    public static void main(String[] args) throws Exception {
+	
+        // Instantiate the ExportManager class
+		ExportManager em = new ExportManager();
+        // Instantiate the ExportConfig class and add the required configurations
+        ExportConfig config = new ExportConfig();
+        config.set("templateFilePath",System.getProperty("user.dir") + "/src/test/java/template_highcharts.html");
+        config.set("type", "pdf");
+        config.set("asyncCapture", true);
+		
+        String[] files = em.export(config, ".", true);
+        
+        System.out.println("Done");
+    }
+}
+
---
+++ examples/ZingChart_Exp.java
@@ -0,0 +1,26 @@
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Dictionary;
+import java.util.HashMap;
+
+import com.fusioncharts.fusionexport.client.*; // import sdk
+
+public class ZingChart_Exp {
+    public static void main(String[] args) throws Exception {
+	
+        // Instantiate the ExportManager class
+		ExportManager em = new ExportManager();
+        // Instantiate the ExportConfig class and add the required configurations
+        ExportConfig config = new ExportConfig();
+        config.set("templateFilePath",System.getProperty("user.dir") + "/src/test/java/template_zingchart.html");
+        config.set("type", "jpg");
+        config.set("asyncCapture", true);
+		
+        String[] files = em.export(config, ".", true);
+        
+        System.out.println("Done");
+    }
+}
+
